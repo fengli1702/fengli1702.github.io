09114525fa8348a7e69b891542ed613efaa26dda
fengli1702 <feng1702@mail.ustc.edu.cn>
2024-03-02 13:32:22 +0800

    3.2

---
+++ index.search.js
@@ -0,0 +1,136 @@
+var relearn_search_index = [
+  {
+    "breadcrumb": "feng web",
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Introduction",
+    "uri": "/introduction/index.html"
+  },
+  {
+    "breadcrumb": "feng web",
+    "content": "文章\n",
+    "description": "",
+    "tags": null,
+    "title": "Post",
+    "uri": "/post/index.html"
+  },
+  {
+    "breadcrumb": "feng web",
+    "content": "\r随机过程随机过程总结\n",
+    "description": "",
+    "tags": null,
+    "title": "note",
+    "uri": "/note/index.html"
+  },
+  {
+    "breadcrumb": "feng web \u003e note \u003e 随机过程",
+    "content": "2月26日 随机过程定义 研究对象：随时间演变的随机现象。一族无穷多个随机变量来描绘。 三元组表示，概率空间、F为概论空间子集，P为F意义下的集函数概率。 X是随机变量，表示F上的映射，将概率空间映射到实数上 首先是随机变量，其次形成一族，一族二元函数 T和S为指标集，为实数，对每一个时间t，均有定义在随机过程上的一个取值在S上的随机变量对应。。随机变量族为一个随机过程 对每一个t，X(t,w)为随机变量，T为参数集(时间指标集)。对固定w，X(t,w)为时刻t时过程的状态，对时间t，X(t)的所有取值为S的随机过程状态空间。 一条随时间变化的随机过程，t的函数，成为一条样本函数。 分类 离散时间，离散状态–群体生长模型 离散时间，连续状态–天气预报，Gauss序列 连续时间，离散状态–Poisson过程 连续时间，连续状态–高斯过程，Brown运动 研究 随机过程的概率结构，统计平均性质\n有限维分布和数字特征 分布函数 对于随机过程\n过程一维分布函数 $ F_t(x)=P(X(t)\\leqslant x) $ 过程一维均值函数 $\\mu_x(t)=EX(t) $ 过程方差函数 $ \\sigma_x^2(t)=Var[X(t)] $ 联合分布函数研究T1和T2两不同时刻的联合二维分布\n自相关函数 $ r_x(t1,t2)=E[X(t_1)X(t_2)] $ 协方差函数 $ R_x(t1,t2)=Cov[X(t_1)X(t_2)] =E{(X(t1)-\\mu_X(t1))(X(t2)-\\mu_X(t2))} $ 以上具有对称性，R(s,t)中st可交换。具有非负定性 随机过程性质 ！并非任意过程都具有全部3条性质\n独立增量：\r$ X(t2)-X(t1),X(t3)-X(t2) $相互独立 平稳增量：任意t，h，\r$ X(t+h)-X(t) $分布与t无关，只与h有关。 Markov性： 略 平稳过程\n任意时间ti和任意h，有\r$ (X(t_1),X(t_2)...)=(X(t_1+h),X(t_2+h)...) $\n宽平稳\n满足二阶矩存在，EX(t)为常数，Rx(t,s)只与时间差t-s有关\n",
+    "description": "2.26",
+    "tags": [
+      "随机过程",
+      "第一节"
+    ],
+    "title": "page 1",
+    "uri": "/note/%E9%9A%8F%E6%9C%BA%E8%BF%87%E7%A8%8B/children1/1/index.html"
+  },
+  {
+    "breadcrumb": "feng web \u003e note \u003e 随机过程",
+    "content": "3月4日 ",
+    "description": "3.4",
+    "tags": [
+      "随机过程",
+      "第二节"
+    ],
+    "title": "page 2",
+    "uri": "/note/%E9%9A%8F%E6%9C%BA%E8%BF%87%E7%A8%8B/children2/2/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "一切的起点 2024-3-2 ",
+    "description": "",
+    "tags": null,
+    "title": "feng web",
+    "uri": "/index.html"
+  },
+  {
+    "breadcrumb": "feng web \u003e Introduction",
+    "content": "this is my wedsite\n",
+    "description": "",
+    "tags": null,
+    "title": "First",
+    "uri": "/introduction/first/index.html"
+  },
+  {
+    "breadcrumb": "feng web \u003e Post",
+    "content": "2024/3/1 3/1日，第一篇博客 ",
+    "description": "",
+    "tags": null,
+    "title": "Second",
+    "uri": "/post/second/index.html"
+  },
+  {
+    "breadcrumb": "feng web \u003e Post",
+    "content": "#Hello\n",
+    "description": "",
+    "tags": null,
+    "title": "First",
+    "uri": "/post/first/index.html"
+  },
+  {
+    "breadcrumb": "feng web",
+    "content": "made by feng\nstudied in ustc\nemail ：feng1702@mail.ustc.edu.cn\"\n",
+    "description": "",
+    "tags": null,
+    "title": "About",
+    "uri": "/about/index.html"
+  },
+  {
+    "breadcrumb": "feng web",
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Categories",
+    "uri": "/categories/index.html"
+  },
+  {
+    "breadcrumb": "feng web",
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Tags",
+    "uri": "/tags/index.html"
+  },
+  {
+    "breadcrumb": "feng web \u003e Tags",
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Tag :: 第一节",
+    "uri": "/tags/%E7%AC%AC%E4%B8%80%E8%8A%82/index.html"
+  },
+  {
+    "breadcrumb": "feng web \u003e Tags",
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Tag :: 第二节",
+    "uri": "/tags/%E7%AC%AC%E4%BA%8C%E8%8A%82/index.html"
+  },
+  {
+    "breadcrumb": "feng web \u003e Tags",
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Tag :: 随机过程",
+    "uri": "/tags/%E9%9A%8F%E6%9C%BA%E8%BF%87%E7%A8%8B/index.html"
+  },
+  {
+    "breadcrumb": "feng web \u003e note",
+    "content": "\rpage 12.26\npage 23.4\n",
+    "description": "随机过程总结",
+    "tags": null,
+    "title": "随机过程",
+    "uri": "/note/%E9%9A%8F%E6%9C%BA%E8%BF%87%E7%A8%8B/index.html"
+  }
+]
